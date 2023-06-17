6742f1a90fab8f51c6ba4eb648b8bcf388687927
Ali Zafar <alizafar194.19@gmail.com>
2023-06-17 01:07:46 +0000

    Implemented basic routing structure with sidemenu & routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import * as Common from "./common";
+
+const Dashboard = React.lazy(() => import("../pages/dashboard"));
+const Contacts = React.lazy(() => import("../pages/contacts"));
+const Invoices = React.lazy(() => import("../pages/invoices"));
+const Team = React.lazy(() => import("../pages/team"));
+
+export const PATH = {
+  HOME: "/",
+  CONTACTS: "/contacts",
+  TEAM: "/team",
+  INVOICES: "/invoices",
+  NOT_FOUND: "*",
+};
+
+const ROUTES = [
+  { path: PATH.HOME, component: <Dashboard /> },
+  { path: PATH.CONTACTS, component: <Contacts /> },
+  { path: PATH.INVOICES, component: <Invoices /> },
+  { path: PATH.TEAM, component: <Team /> },
+];
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {ROUTES.map((route) => {
+        const { path, component } = route;
+        return (
+          <Route
+            path={path}
+            element={
+              <React.Suspense fallback={<Common.FallBack />}>
+                {component}
+              </React.Suspense>
+            }
+          />
+        );
+      })}
+      <Route path={PATH.NOT_FOUND} element={<Common.NoMatch />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
